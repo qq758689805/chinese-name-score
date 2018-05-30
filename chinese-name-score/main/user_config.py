82cbd0405bb7696d84d6c27de6f3aa667ff7060c
qq758689805 <758689805@qq.com>
2018-05-30 09:38:29 +0800

    create project

---
+++ chinese-name-score/main/user_config.py
@@ -0,0 +1,34 @@
+# coding:GB18030
+
+"""
+在这里写好配置
+"""
+
+setting = {}
+
+# 限定字，如果配置了该值，则会取用单字字典，否则取用多字字典
+setting["limit_world"] = ""
+# 姓
+setting["name_prefix"] = "夏"
+# 性别，取值为 男 或者 女
+setting["sex"] = "女"
+# 省份
+setting["area_province"] = "四川"
+# 城市
+setting["area_region"] = "成都"
+# 出生的公历年份
+setting['year'] = "2018"
+# 出生的公历月份
+setting['month'] = "9"
+# 出生的公历日子
+setting['day'] = "25"
+# 出生的公历小时
+setting['hour'] = "9"
+# 出生的公历分钟
+setting['minute'] = "50"
+# 结果产出文件名称
+setting['output_fname'] = "names_boys_source_shenxing.txt"
+
+
+
+
